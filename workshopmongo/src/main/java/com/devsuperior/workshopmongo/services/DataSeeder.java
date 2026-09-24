356e3c94f9d4b873858e6d5215e51d086e4c3414
Lev_TheDev <levinosecretti@outlook.com>
2026-09-23 22:51:12 -0400

    Implementing UserDTO.

--- workshopmongo/src/main/java/com/devsuperior/workshopmongo/services/DataSeeder.java
+++ workshopmongo/src/main/java/com/devsuperior/workshopmongo/services/DataSeeder.java
@@ -3,16 +3,18 @@ package com.devsuperior.workshopmongo.services;
 import com.devsuperior.workshopmongo.domain.User;
 import com.devsuperior.workshopmongo.repository.UserRepository;
 import net.datafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 @Component
 public class DataSeeder implements CommandLineRunner {
-
+    @Autowired
     private final UserRepository rep;
 
     public DataSeeder(UserRepository rep) {
@@ -21,9 +23,17 @@ public class DataSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Scanner sc = new Scanner(System.in);
         if (rep.count() > 0) {
-            System.out.println("-> Banco de dados já possui registros. Pulando seeding.");
-            return;
+            System.out.println("-> Banco de dados já possui registros. Deseja limpar o banco de dados? (s/n)");
+            String answer = sc.next();
+            if (answer.equalsIgnoreCase("s")) {
+                rep.deleteAll();
+                System.out.println("-> Banco de dados limpo com sucesso!");
+            } else {
+                System.out.println("-> Operação cancelada. Nenhum dado foi alterado.");
+                return;
+            }
         }
 
         System.out.println("-> Iniciando a geração de dados falsos com Datafaker...");
@@ -31,7 +41,6 @@ public class DataSeeder implements CommandLineRunner {
         List<User> users = new ArrayList<>();
 
         int nUsers = 10;
-
         for (int i = 0; i < nUsers; i++) {
             String name = faker.name().fullName();
             String email = faker.internet().emailAddress();
